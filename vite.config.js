7d86de6130d470e106b7669141af52c070431912
Cassandra <cassandra.moore1516@gmail.com>
2025-11-12 13:57:53 -0600

    Update vite.cnfig.js to allowe Hoste

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5174,
-    preview: {
-      allowedHosts: ['cds-ys0t.onrender.com']
-    }
+    port: 5174
+  },
+  preview: {
+    allowedHosts: ['cds-ys0t.onrender.com']
   }
 })
